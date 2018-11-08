5601ada6a0d44078d78c2d7504da555044711cd0
Kharade, Kiran <Kiran.kharade@klingelnberg.com>
2018-11-08 18:04:57 +0530

    Added classes for Leave and Regularization

---
+++ NetCore/Models.Core/HR/Attendance/Leave.cs
@@ -0,0 +1,51 @@
+﻿using Models.Core.Helpers;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Core.HR.Attendance
+{
+    public enum LeaveType
+    {
+        PersonalLeave,
+        SickLeave,
+        PaternalLeave,
+        MaternalLeave,
+        CompensatoryOff,
+        EducationalLeave,
+        Sabattical
+    }
+
+    public enum LeaveStatus
+    {
+        Applied,
+        Approved,
+        Rejected
+    }
+
+    public class Leave
+    {
+        [BsonId]
+        [JsonConverter(typeof(ObjectIdConverter))]
+        public ObjectId _objetId { get; set; }
+
+        public int ID { get; set; }
+
+        [BsonDateTimeOptions]
+        public DateTime StartDate { get; set; }
+
+        [BsonDateTimeOptions]
+        public DateTime EndDate { get; set; }
+
+        public int EmployeeID { get; set; }
+
+        public LeaveType LeaveType { get; set; }
+
+        public LeaveStatus LeaveStatus { get; set; }
+
+        public string Description { get; set; }
+    }
+}

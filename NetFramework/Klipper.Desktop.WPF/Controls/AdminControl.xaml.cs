949eaa2cdf2bd7e910c39259fbf5c0e04742daab
Naik_San <snketnaikredkar@gmail.com>
2018-11-29 16:08:15 +0530

    Images added for Gender

--- NetFramework/Klipper.Desktop.WPF/Controls/AdminControl.xaml.cs
+++ NetFramework/Klipper.Desktop.WPF/Controls/AdminControl.xaml.cs
@@ -1,5 +1,11 @@
-﻿using Models.Core.Employment;
+﻿using Klipper.Desktop.WPF.CustomControls;
+using Models.Core.Employment;
 using Newtonsoft.Json;
+using Sparkle.Appearance;
+using Sparkle.Controls.Buttons;
+using Sparkle.Controls.Dialogs;
+using Sparkle.Controls.Panels;
+using Sparkle.DataStructures;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,47 +30,115 @@ namespace Klipper.Desktop.WPF.Controls
     public partial class AdminControl : UserControl
     {
         HttpClient client = new HttpClient();
-        
         public AdminControl()
         {
             InitializeComponent();
             client.BaseAddress = new Uri("https://localhost:6001/");
-            IDTextBox.Text = GetMaxEmployeeId();
+            TheMenu.CollapsedWidth = 50.0;
+            TheMenu.ExpandedWidth = 220.0;
+            TheMenu.SelectedIndex = 0;
+            TheMenu.Expand();
+            dataGrid1.ItemsSource = Task.Run(async ()=> await GetAllEmployeesAsync()).Result;
         }
 
-        private void ComboBox_Selected(object sender, RoutedEventArgs e)
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-
+            LoadMenuItems();
+            var data = Task.Run(async () => await GetAllEmployeesAsync()).Result;
         }
 
-        private void Save_EmployeeData(object sender, RoutedEventArgs e)
+        private void LoadMenuItems()
         {
-            Employee employeeData = new Employee()
-            {
-                FirstName = FirstNameTextBox.Text,
-                LastName = LastNameTextBox.Text,
-                ID = Convert.ToInt32(IDTextBox.Text)
+            var strs = new Dictionary<string, string>() {
+                { "User Management", "Continue_disabled.png" },
+                { "Settings", "Continue_disabled.png" },
             };
 
-            string jsonData = JsonConvert.SerializeObject(employeeData, Formatting.Indented);
-            StringContent httpContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            
-            HttpResponseMessage response = client.PostAsync("/api/Employees/", httpContent).Result;
-            if (response.IsSuccessStatusCode)
+            foreach (var k in strs.Keys)
             {
-                MessageBox.Show("Employee Information Saved Successfully !","Saved",MessageBoxButton.OK, MessageBoxImage.Information);
-                IDTextBox.Text = GetMaxEmployeeId();
-            }
-            else
-            {
-                MessageBox.Show($"Error while saving:{response.RequestMessage}", "Error", MessageBoxButton.OK,MessageBoxImage.Error);
+                var imageName = strs[k];
+                var tab = new SelectableItem(k, GetControl(imageName), "./Images/Generic/" + imageName)
+                {
+                    IconHeight = 35,
+                    IconWidth = 35,
+                    ItemHeight = 50
+                };
+                TheMenu.AddMenuItem(tab);
             }
         }
 
-        private string GetMaxEmployeeId()
+        private ContentControl GetControl(string imageName)
         {
-            var response = client.GetAsync("api/Employees/GetMaxEmployeeId").Result;
-            return "";
+            var imageSource = (ImageSource)new ImageSourceConverter().ConvertFromString("./Images/Generic/" + imageName);
+            var image = new Image()
+            {
+                Source = imageSource,
+                Width = 290
+            };
+
+            var b = new Border()
+            {
+                VerticalAlignment = VerticalAlignment.Stretch,
+                Child = image,
+                CornerRadius = new CornerRadius(0),
+                Margin = new Thickness(0),
+                Padding = new Thickness(0),
+                Background = new SolidColorBrush(Color.FromRgb(45, 55, 65)),
+            };
+            return new ContentControl()
+            {
+                Content = b,
+                VerticalAlignment = VerticalAlignment.Stretch,
+                VerticalContentAlignment = VerticalAlignment.Stretch
+            };
+
+        }
+
+        private void OnMenuSelectionChanged(object sender, SelectableItemSelectionChangedEventArgs e)
+        {
+            var item = e.Current;
+            InteractionArea.Content = item.AssociatedContent;
+            item.AssociatedContent.IsEnabled = false;
+            TheMenu.ForceResize();
+        }
+
+        private async Task<IQueryable> GetAllEmployeesAsync()
+        {
+            HttpResponseMessage response = client.GetAsync("/api/Employees").Result;
+            string jsonString = await response.Content.ReadAsStringAsync();
+            IQueryable jsonData = JsonConvert.DeserializeObject<Employee[]>(jsonString)
+                .Select(x=> new {
+                        x.FirstName,
+                        x.LastName,
+                        x.BirthDate,
+                        x.Email
+                    }
+                ).AsQueryable();
+            return jsonData;
+        }
+
+
+        private void AddEmployee_click(object sender, RoutedEventArgs e)
+        {
+            GenericDialog dlg = new GenericDialog();
+            dlg.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            dlg.CollapseBottomRegion();
+            dlg.ShowCloseButton = true;
+            dlg.Header = "Add New Employee";
+            AppearanceManager.SetAppearance(dlg);
+
+            var cp = new BasicDialogPanel();
+            var control = new AddEmployeeControl();
+            cp.Container.Content = control;
+
+            dlg.SetDialogRegion(cp);
+            dlg.DialogClosed += (s, args) => { dlg.Close(); };
+            
+            var btn = new PanelButton() { ButtonWidth = 150, ButtonText = "close!" };
+            btn.Clicked += (s, args) => { dlg.Close(); };
+            dlg.AddButton(btn);
+            dlg.ShowDialog();
+            //AddChild(dlg);
         }
     }
 }
---
+++ NetFramework/Klipper.Desktop.WPF/CustomControls/AddEmployeeControl.xaml.cs
@@ -0,0 +1,140 @@
+﻿using Models.Core;
+using Models.Core.Employment;
+using Newtonsoft.Json;
+using Sparkle.Controls.Dialogs;
+using Sparkle.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Klipper.Desktop.WPF.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for AddEmployeeControl.xaml
+    /// </summary>
+    public partial class AddEmployeeControl : UserControl
+    {
+        public event EventHandler Closed = null;
+        private HttpClient _client = new HttpClient();
+
+        public ObservableCollection<SelectableItem> Items { get; } = new ObservableCollection<SelectableItem>()
+        {
+            new SelectableItem("M", null),
+            new SelectableItem("F", null),
+        };
+        private int _SelectedIndex = 0;
+        public int SelectedIndex
+        {
+            get { return _SelectedIndex; }
+            set
+            {
+                _SelectedIndex = value;
+                OnPropertyChanged();
+            }
+        }
+        public SelectableItem SelectedItem
+        {
+            get
+            {
+                if (SelectedIndex >= 0 && SelectedIndex < Items.Count)
+                    return Items[SelectedIndex];
+                return null;
+            }
+            set
+            {
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+       
+        public AddEmployeeControl()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseDialog_clicked(object sender, RoutedEventArgs e)
+        {
+            Closed?.Invoke(this, null);
+        }
+        private static int _selectedItem { get; set; } = 1;
+
+        private async void AddEmployee_ClickAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                Employee empData = new Employee()
+                {
+                    ID = Convert.ToInt32(IDTextBox.Text),
+                    FirstName = FirstNameTextBox.Text,
+                    LastName = LastNameTextBox.Text,
+                    Gender = (Gender)_selectedItem,
+                    Prefix = PrefixComboBox.Text,
+                    Title = TitleTextBox.Text,
+                    BirthDate = (DateTime)BirthdateTextBox.SelectedDate,
+                    Email = EmailTextBox.Text,
+                    MobilePhone = MobilenumberTextBox.Text,
+                    WorkPhone = WorkPhoneTextBox.Text,
+                    ProvidentFundNumber = PFNumberTextBox.Text,
+                    ProvidentFundUANNumber = PFUANNumberTextBox.Text,
+                    PANNumber = PanNumberTextBox.Text,
+                    AadharNumber = AadharNumberTextBox.Text,
+                    JoiningDate = DateTime.UtcNow,
+                };
+
+                _client.BaseAddress = new Uri("https://localhost:6001/");
+                string json = JsonConvert.SerializeObject(empData, Formatting.Indented);
+                var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await _client.PostAsync("/api/Employees/", httpContent);
+                response.EnsureSuccessStatusCode();
+                MessageDialog.Show("Success Message", "Employee saved successsfully !!", true, "Close", false, "", DialogFlavour.Information, true);
+            }
+            catch (Exception exp)
+            {
+                MessageDialog.Show("Error Message", exp.Message, true, "Close", false, "", DialogFlavour.Error, true);
+            }
+        }
+
+        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+        private static bool IsTextAllowed(string text)
+        {
+            Regex _regex = new Regex("[^0-9.-]+");
+            return !_regex.IsMatch(text);
+        }
+
+        private void MyRadioTrain_Loaded(object sender, RoutedEventArgs e)
+        {
+            MyRadioTrain.ItemSelectionChanged += ItemSelectionChanged;
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ItemSelectionChanged(object sender, SelectableItemSelectionChangedEventArgs args)
+        {
+            string header = args.Current.Header;
+            _selectedItem = header == "M" ? 1 : 2;
+            OnPropertyChanged(nameof(_selectedItem));
+        }
+
+    }
+}
